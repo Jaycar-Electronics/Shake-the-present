c751907cd2fa7ed730b73cabe4df15fd99b677b7
Jaycar-Electronics <github@jaycar.com.au>
2020-11-16 11:15:39 +1100

    some framework

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
 # Shake the present
- Shake the box to find what you get
+
+ Shake the box to find what you get in this magic 8-ball styled christmas gift game.
 
 ## Bill of Materials
 
+| Qty | Code     | Description                  |
+| --- | -------- | ---------------------------- |
+| 1   | {XC4410} | Arduino Uno compatible board |
+| 1   | {XC3728} | 1.3" Monochrome OLED         |
+| 1   | {XC3732} | Tri Axis Digital Tilt Sensor |
+| 1   | {WC6028} | Plug socket leads            |
+| 1   | {PH9251} | Battery DC lead              |
+| 1   | {SB2423} | Battery                      |
 
 
+## How to build
+<div id='instructions'>
+For instructions on how to build, check out: https://jaycar.com.au/gift-guessing-game
+</div>
 
-## Wiring connections
+## Working principle
 
-### OLED
+The basis of this is using the {} Tri Axis tilt sensor to detect the magnitude of acceleration that the box is going under
 
+We use [Pyagrath's Theorem]() to find the total magnitude, and use a [Rolling Average]() to smooth out spikes in the data, so a sharp knock will not trip the shake counter (as the spike in data will be smoothed out). If you want to see this in action, uncomment the `RAW_LOG` define near the top of the code.
+
+After about 5 shakes, we show another image on the OLED, and after 10 shakes, we generate a random number and "roll" to the gift that the person gets.
+
+### Memory Limitations
+
+There's very limited memory on the ATMEGA 328P chip, which isn't made any easier with the variety of feature-full libraries that are available through the library manager. For this project, the memory space very quickly ran out of room trying to manage both the accelometer and oled objects, causing a reboot once it reaches to a certain point in the program.
+
+To overcome this we used some resource allocation in our core methods: `draw()` and `measureShake()`.
+
+Here, each method _creates_ the object that it is managing. For instance, the `draw()` function  creates the `U8g2lib` object, draws things on the screen, and then deletes the object, thus freeing the memory. Similarly, the `measureShake()` will create the accelerometer management object, take readings, and store it in the buffer to be averaged later, before deleting the accelometer object again.
+
+In many applications this is slow to do, but for our purposes, it actually functions fine and utilises more of the speed that is available for us via the ATMEGA328P.
--- shake/shake.ino
+++ shake/shake.ino
@@ -13,6 +13,7 @@
 #include "shake_image.h"
 // Have a look at the readme file for how to build the bitmap images.
 
+//#define RAW_LOG
 
 /*************** Constants *****************************************
 * and other defines
@@ -23,35 +24,32 @@ const short shake_threshold = 1400; //the threshold that the shake must overcome
 
 void (*resetSelf)(void) = 0; //shortcut to reset the arduino.
 
-enum GameState {    //internal state tracking. (State Machine)
-    initial,
-    keep_shaking,
-    reveal
+enum GameState
+{ //internal state tracking. (State Machine)
+  initial,
+  keep_shaking,
+  reveal
 };
 
 //function definitions:
-int measureShake();           //get the shake magnitured
+int measureShake();             //get the shake magnitured
 short getAverageMagnitude(int); //get average shake, without recording new values
-void draw(GameState state);   // draw images on the screen.
-
-
-
+void draw(GameState state);     // draw images on the screen.
 
 /*************** Global vars ******************************************
 */
 
-uint8_t shake_counter = 0;    //count how many times we've shaken it
+uint8_t shake_counter = 0;                      //count how many times we've shaken it
 short magnitude_history[mag_history_max] = {0}; //the average window function as above
-uint8_t mag_idx = 0;          //internal counter to spots in the mag_history
+uint8_t mag_idx = 0;                            //internal counter to spots in the mag_history
+int raw_mag = 0;
 
+bool tripped = false; //have we reached high accel?
+bool dirty = true;    //do we need to re-draw the screen?
 
-bool tripped = false;         //have we reached high accel?
-bool dirty = true;            //do we need to re-draw the screen?
+uint8_t lastShake = 0; //for debugging;
 
-uint8_t lastShake = 0;        //for debugging;
-
-GameState state = initial;    //start at the initial state
-
+GameState state = initial; //start at the initial state
 
 /*************** Setup and Loop ********************************************
 * Main routines
@@ -62,6 +60,9 @@ void setup()
   Serial.begin(9600);
   Wire.begin();
   randomSeed(analogRead(0));
+#ifdef RAW_LOG
+  Serial.println("raw, avg, thresh, count");
+#endif
 }
 
 void loop()
@@ -73,7 +74,8 @@ void loop()
     dirty = false;
   }
 
-  else if (state == reveal){
+  else if (state == reveal)
+  {
     draw_reveal();
     return;
   }
@@ -82,30 +84,40 @@ void loop()
   short shake = measureShake();
 
   //------- check shakes ----------------
-  if (shake > shake_threshold && !tripped){
+  if (shake > shake_threshold && !tripped)
+  {
     tripped = true;
     shake_counter++;
     Serial.println("Tripped!");
   }
-  else if (shake < shake_threshold && tripped){
+  else if (shake < shake_threshold && tripped)
+  {
     tripped = false;
   }
   //------------------------------------
 
-
   //------- check state ----------------
-  if(shake_counter > 10 && state != reveal){
+  if (shake_counter > 10 && state != reveal)
+  {
     state = reveal;
     dirty = true;
-  } else if (shake_counter > 5 && state != keep_shaking){
+  }
+  else if (shake_counter > 5 && state != keep_shaking)
+  {
     state = keep_shaking;
     dirty = true;
   }
   //------------------------------------
   //now it will run the loop again, draw if needed.
+
+  // This is just debugging to help without
+  // spamming the serial monitor. If you want
+
+#ifndef RAW_LOG
   lastShake = max(shake, lastShake);
 
-  if((shake+100) < lastShake && lastShake > 0){
+  if ((shake + 100) < lastShake && lastShake > 0)
+  {
     Serial.print("Shake Max: ");
     Serial.print(lastShake);
     Serial.print(" Thresh: ");
@@ -114,14 +126,25 @@ void loop()
     Serial.println(shake_counter);
     lastShake = 0;
   }
-}
 
+#else
+  Serial.print(raw_mag);
+  Serial.print(",");
+  Serial.print(shake);
+  Serial.print(",");
+  Serial.print(shake_threshold);
+  Serial.print(",");
+  Serial.print(shake_counter * 100);
+  Serial.println();
+#endif
+}
 
 /*************** Private functions *******************************************
 * Accelerometer and OLED
 */
 
-void draw(GameState state){
+void draw(GameState state)
+{
   /* We use this function
   * To create the OLED management object
   * and draw on the screen.
@@ -147,20 +170,23 @@ void draw(GameState state){
   //oled.setFontDirection(0);
   oled.firstPage();
 
-  do{
+  do
+  {
 
-    if (state == initial){
+    if (state == initial)
+    {
       oled.drawXBMP(0, 0, 128, 64, santa_image_bits);
     }
-    else if ( state == keep_shaking) {
+    else if (state == keep_shaking)
+    {
       //oled.drawStr(20, 40, "Keep shaking");
       oled.drawXBMP(0, 0, 128, 64, shake_image_bits);
     }
   } while (oled.nextPage());
 }
 
-
-void draw_reveal(){
+void draw_reveal()
+{
   /* This function is very similar to the above
   *  Except with the random "dice roll" effect
   *  to show what the user is getting.
@@ -183,56 +209,59 @@ void draw_reveal(){
   char buffer[20];
   uint8_t x = random(0, presents_count); //randomly pick from the entire range
 
-  for(uint8_t i = 0; i < 20; i++){
+  for (uint8_t i = 0; i < 20; i++)
+  {
 
-    uint8_t val = (x+i) % presents_count; //scroll though
+    uint8_t val = (x + i) % presents_count; //scroll though
 
     //get a pointer to the string at this position
-    char * ptr = (char*) pgm_read_word( &(presents [val]));
+    char *ptr = (char *)pgm_read_word(&(presents[val]));
 
     //copy it into the buffer
     strcpy_P(buffer, ptr);
 
-
     oled.firstPage();
-    do{
-        oled.drawStr(30, 10, "You will get:");
-        uint8_t x = 0;
-        uint8_t y = 33;
-        oled.drawHLine(0, 17, 128);
-        oled.drawHLine(0, 20, 128);
+    do
+    {
+      oled.drawStr(30, 10, "You will get:");
+      uint8_t x = 0;
+      uint8_t y = 33;
+      oled.drawHLine(0, 17, 128);
+      oled.drawHLine(0, 20, 128);
 
-        for(uint8_t c = 0; buffer[c] != 0; c++){
-          // we want to draw line by line so that we can handle newlines
-          // easier.
+      for (uint8_t c = 0; buffer[c] != 0; c++)
+      {
+        // we want to draw letter by letter so that we can handle newlines
+        // easier.
 
-          if(buffer[c]=='\n'){
-            x = 5; y = 45;
-          }
-
-          oled.setCursor(x,y);
-          oled.print(buffer[c]);
-
-          x+=6;
+        if (buffer[c] == '\n')
+        {
+          x = 5;
+          y = 45;
         }
-        oled.drawHLine(0, 50, 128);
-        oled.drawHLine(0, 52, 128);
+
+        oled.setCursor(x, y);
+        oled.print(buffer[c]);
+
+        x += 6; //one letter width
+      }
+      oled.drawHLine(0, 50, 128);
+      oled.drawHLine(0, 52, 128);
     } while (oled.nextPage());
 
-
     //a semi nice exponential function to slow down dice roll.
-    //given it's a micro, don't ask for too much.
+    // really happy for someone to do a nicer "slow roullette"
+    // effect
+    // given it's a microcontroller, don't ask for too much.
     float frac = i / 6;
-    delay( 100 * pow(frac, 2));
+    delay(100 * pow(frac, 2));
   }
-
 
   //we finally "rest" on a position for 10 seconds, to show the user
   // then reset.
   delay(10000L);
   resetSelf();
 }
-
 
 short getAverageMagnitude(int mag)
 {
@@ -247,8 +276,8 @@ short getAverageMagnitude(int mag)
   return sum / mag_history_max;
 }
 
-
-int measureShake(){
+int measureShake()
+{
   MMA8452Q accel;
   if (!accel.begin())
   {
@@ -259,15 +288,16 @@ int measureShake(){
   }
 
   while (!accel.available())
-  ;
+    ;
 
-  short v = getAverageMagnitude(sqrt(
+  raw_mag = sqrt(
       pow(accel.getX(), 2) +
       pow(accel.getY(), 2) +
-      pow(accel.getZ(), 2)));
+      pow(accel.getZ(), 2));
 
-  return v;
+  short v = getAverageMagnitude(raw_mag)
+
+      return v;
 
   //after this point the accel is removed from memory
 }
-
